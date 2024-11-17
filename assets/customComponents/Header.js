5e7c0813b18c962504088ffd7675b7f3d3554e59
Sergio Maldonado <ser.mal.vil@gmail.com>
2024-11-16 19:35:28 -0600

    Initial commit with working version

---
+++ assets/customComponents/Header.js
@@ -0,0 +1,79 @@
+class Header extends HTMLElement {
+  constructor() {
+    super()
+  }
+
+  connectedCallback() {
+    // const shadow = this.attachShadow({ mode: "open" })
+
+    const links = [
+      { nombre: "Home", link: "./" },
+      { nombre: "People", link: "./people.html" },
+      { nombre: "Research", link: "./research.html" },
+      { nombre: "Publications", link: "./publications.html" },
+      { nombre: "Teaching", link: "./teaching.html" },
+      { nombre: "Join us", link: "./join-us.html" },
+    ]
+
+    const wrapper = document.createElement("header")
+    wrapper.classList.add(
+      "d-flex",
+      "justify-content-between",
+      "bg-1",
+      "p-3",
+      "align-items-center"
+    )
+    wrapper.innerHTML = `
+      <span class="text-white fs-4">
+        Sergio Maldonado - Environmental Fluid Mechanics
+      </span>
+      <nav class="d-none d-lg-block">
+        <ul class="d-flex list-unstyled mb-0">
+          ${links
+            .map(
+              ({ nombre, link }) => `
+            <li>
+              <a href="${link}" class="text-decoration-none text-white me-3">
+                ${nombre}
+              </a>
+            </li> 
+          `
+            )
+            .join("")}
+        </ul>
+      </nav>
+      <i id="menu-burger" class="bi bi-list text-white fs-3 d-lg-none" role="button" tabindex="0"></i>
+      <div id="menu-cel" class="position-fixed top-0 left-0 w-75 h-100 bg-1 p-3 d-none z-3">
+        <nav>
+          <ul class="p-0 m-0">
+            ${links
+              .map(
+                ({ nombre, link }) => `
+            <li class="mb-3">
+              <a href="${link}" class="text-decoration-none text-white fs-3">
+                ${nombre}
+              </a>
+            </li>
+          `
+              )
+              .join("")}
+          </ul>
+        </nav>
+      </div>
+    `
+    // shadow.appendChild(wrapper)
+    const btn = wrapper.querySelector("#menu-burger")
+    const menuCel = wrapper.querySelector("#menu-cel")
+    btn.onclick = () => {
+      const hasDnone = menuCel.classList.contains("d-none")
+      if (hasDnone) {
+        menuCel.classList.remove("d-none")
+      } else {
+        menuCel.classList.add("d-none")
+      }
+    }
+    this.appendChild(wrapper)
+  }
+}
+
+customElements.define("yoyo-header", Header)
---
+++ js/researchers.js
@@ -0,0 +1,86 @@
+const ulResearchers = document.querySelector("#researchers")
+
+class Researcher {
+  constructor(nombre, img, link, body) {
+    this.nombre = nombre
+    this.img = img || "../assets/images/unknown_profile.jpg"
+    this.link = link
+    this.body = body
+  }
+}
+
+const researchers = [
+  new Researcher(
+    "Xinyang Ge, Ph.D.",
+    "",
+    "",
+    [
+      "Postdoctoral Researcher",
+      "Experimental Fluid Mechanics",
+      "University of Southampton, UK",
+    ]
+  ),
+  new Researcher("Will Crawford-Jones", "", "", [
+    "Ph.D. Student",
+    "Particle-fluid interactions",
+    "University of Southampton, UK",
+  ]),
+  new Researcher("Laura Coombs", "", "", [
+    "Ph.D. Student",
+    "Fluid-cell (algae) interactions",
+    "University of Southampton, UK",
+  ]),
+  new Researcher("Nahum Banks", "", "", [
+    "Ph.D. Student",
+    "Physics-informed AI",
+    "University of Southampton, UK",
+  ]),
+  new Researcher("Nastja Vodopivec", "", "", [
+    "Ph.D. Student",
+    "Physics-informed AI",
+    "University of Southampton, UK",
+  ]),
+  new Researcher("Henry Davies", "", "", [
+    "MEng (Aero) Student",
+    "Physics-informed AI",
+    "University of Southampton, UK",
+  ]),
+  new Researcher("Fabián Hernández R.", "", "", [
+    "BSc (Physics) Student",
+    "CFD and Physics-informed AI",
+    "Tecnológico de Monterrey, Mex.",
+  ]),
+  new Researcher("Iván A. Chávez R.", "", "", [
+    "BSc (Physics) Student",
+    "Physics-informed AI",
+    "Tecnológico de Monterrey, Mex.",
+  ]),
+]
+
+const stringResearchers = researchers
+  .map(
+    ({ nombre, img, link, body }) => `
+
+        <li class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-5">
+          <img src="${img}" alt="" class="w-75 mb-3 d-block mx-auto" />
+          ${
+            !link
+              ? `
+                <span class="d-block fw-bold fs-5 text-center mb-3 color1">${nombre}</span>
+              `
+              : `
+                <a
+                  class="d-block fw-bold fs-5 text-center mb-3 color1"
+                  href="${link}"
+                  target="_blank"  
+                >${nombre}</a>
+              `
+          }
+          ${body.map((p) => `<p class="mb-0">${p}</p>`).join("")}
+        </li>
+ 
+    `
+  )
+  .join("")
+
+ulResearchers.innerHTML = stringResearchers
