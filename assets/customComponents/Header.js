fa3aef0abceeda95f7abff4d354eeea11cd0088b
Omar Maldonado <omaldonado@apexsystems.com>
2024-11-17 09:27:22 -0600

    ajustes para vista responsiva

--- assets/customComponents/Header.js
+++ assets/customComponents/Header.js
@@ -24,9 +24,14 @@ class Header extends HTMLElement {
       "align-items-center"
     )
     wrapper.innerHTML = `
-      <span class="text-white fs-4">
-        Sergio Maldonado - Environmental Fluid Mechanics
-      </span>
+      <div>
+        <span class="text-white fs-4">
+          Sergio Maldonado -
+        </span>
+        <span class="text-white fs-5">
+          Environmental Fluid Mechanics
+        </span>
+      </div>
       <nav class="d-none d-lg-block">
         <ul class="d-flex list-unstyled mb-0">
           ${links
@@ -42,7 +47,7 @@ class Header extends HTMLElement {
             .join("")}
         </ul>
       </nav>
-      <i id="menu-burger" class="bi bi-list text-white fs-3 d-lg-none" role="button" tabindex="0"></i>
+      <i id="menu-burger" class="bi bi-list text-white fs-3 d-lg-none ms-2" role="button" tabindex="0"></i>
       <div id="menu-cel" class="position-fixed top-0 left-0 w-75 h-100 bg-1 p-3 d-none z-3">
         <nav>
           <ul class="p-0 m-0">
